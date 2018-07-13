2b6fc757e23abb84347c2aed1c98cdac07e648e3
v20100v <7567933+v20100v@users.noreply.github.com>
2018-07-13 15:29:40 +0200

    Jekyll website published

--- _docs/publish-docs-site.bat
+++ _docs/publish-docs-site.bat
@@ -17,6 +17,7 @@ echo.
 echo ---[ Step 1/4 - Clean output folder ] ---
 echo * Attempt to remove : "%FOLDER_BUILD%"
 rmdir %FOLDER_BUILD% /s /q
+echo ------------------------------------------
 
 echo.
 echo.
@@ -24,24 +25,46 @@ echo.
 echo ---[ Step 2/4 - Build Jekyll site ] ---
 echo * Attempt to build Jekyll website
 call bundle exec jekyll build
+echo ------------------------------------------
 
 echo.
 echo.
 
 echo ---[ Step 3/4 - Copy Jekyll site in the root ] ---
 echo * Attempt to copy Jekyll website in the root of project
-echo xcopy "%FOLDER_BUILD%" "%FOLDER_ROOT_GIT%" /E /H /Y
-xcopy "%FOLDER_BUILD%" "%FOLDER_ROOT_GIT%" /E /H /Y
+echo.
+echo * Create the file xcopy_EXCLUDE.txt in order to ignore some file and directory.
+echo github_jekyll.txt > xcopy_Exclude.txt
+echo .bat >> xcopy_Exclude.txt
+echo * The file xcopy_EXCLUDE.txt is created.
+echo.
+echo * Launch command : xcopy "%FOLDER_BUILD%" "%FOLDER_ROOT_GIT%" /E /H /Y /EXCLUDE:xcopy_Exclude.txt
+xcopy "%FOLDER_BUILD%" "%FOLDER_ROOT_GIT%" /E /H /Y /EXCLUDE:xcopy_Exclude.txt
+echo * Files and directory are copied.
+echo.
+echo * Delete xcopy_Exclude.txt.
+del xcopy_Exclude.txt
+echo ------------------------------------------
 
 echo.
 echo.
 
 echo ----[ Step 4/4 - Published on Github  ] ----
+echo.
+echo * Attempt to stage all files
 git add --all
-git commit -m "Jekyll website published"
-git push origin master
 
+echo.
+echo * Attempt to commit staged files
+git commit -m "Jekyll website published"
+
+echo.
+echo * Attempt to push on Gituhb
+git push origin master
+echo ------------------------------------------
+
+echo.
+echo.
 
 echo ----[ End process ]----
 echo.
-pause
