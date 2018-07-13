b663c263c88b53bc8aaeb8a83cce0d9bf6756a4f
v20100v <7567933+v20100v@users.noreply.github.com>
2018-07-13 14:43:01 +0200

    Improve published task

--- _docs/publish-docs-site.bat
+++ _docs/publish-docs-site.bat
@@ -1,11 +1,12 @@
 cls
 @echo off
 
-chcp 65001
+@chcp 65001
 cd /d "%~dp0"
 
 set FOLDER_CURRENT=%cd%
 set FOLDER_BUILD=%FOLDER_CURRENT%\_site
+set FOLDER_ROOT_GIT=%cd%\..\
 
 
 type github_jekyll.txt
@@ -18,20 +19,20 @@ rmdir %FOLDER_BUILD% /s /q
 echo.
 echo ---[ Step 2/5 - Build Jekyll site ] ---
 echo * Attempt to build Jekyll website
-bundle exec jekyll build
+call bundle exec jekyll build
 
 echo.
 echo ---[ Step 3/5 - Copy Jekyll site in the root ] ---
 echo * Attempt to copy Jekyll website in the root of project
-xcopy "%FOLDER_BUILD%" "%FOLDER_CURRENT%" /E /H /Y
+xcopy "%FOLDER_BUILD%" "%FOLDER_ROOT_GIT%" /E /H /Y
 
 echo.
 echo ---[ Step 4/5 - Git commit ] ---
-git --git-dir=.git add --all
-git --git-dir=.git commit -m "Jekyll website published"
+git --git-dir=%FOLDER_ROOT_GIT%/.git add --all
+git --git-dir=%FOLDER_ROOT_GIT%/.git commit -m "Jekyll website published"
 
 echo.
 echo ---[ Step 5/5 - Git push ] ---
-git --git-dir=.git push origin master
+git --git-dir=%FOLDER_ROOT_GIT%/.git push origin master
 
 pause
